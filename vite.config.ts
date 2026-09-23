efd22f737f36d1a74ecd8eb660475f782ee5ae7e
Jeremiah King <jeremiah@vols.utk.edu>
2026-09-23 10:51:52 -0400

    added base to vitr.config.ts. added .github/workflows/deploy.yml.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/3d-global-heatmap/",
   plugins: [react()],
   test: {
     environment: "jsdom",

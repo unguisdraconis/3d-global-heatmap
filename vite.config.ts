798c7960525d7955207146e3f0b7e2c367a9e8c3
Jeremiah King <jeremiah@vols.utk.edu>
2026-09-24 03:23:39 -0400

    changed vite base to "temperies".

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/3d-global-heatmap/",
+  base: "/temperies/",
   plugins: [react()],
   test: {
     environment: "jsdom",
